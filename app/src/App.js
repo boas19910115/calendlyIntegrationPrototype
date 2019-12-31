90f7798f130bb6f8bf0906f3e0fc52fea67c36ec
Yuehnan,Wu <yuehnan.wu@imersive.com>
2019-12-31 16:46:09 +0800

    fix name

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.scss'
+import { Calendly } from 'containers/Calendly'
+
+function App () {
+  return (
+    <div className='App'>
+      <Calendly />
+    </div>
+  )
+}
+
+export default App
